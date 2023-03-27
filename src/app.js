9e0f5a5fa0938274dcedcfc6d38f47a468df5f29
장용호 <yongho.jang@datamaker.io>
2023-03-27 14:05:41 +0900

    feat: delete app.js

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import utils from '/src/assemble/utils/utils.js'
-import canvas from '/src/assemble/canvas/canvas.js'
-import toolkit from '/src/assemble/toolkit/toolkit.js'
-
-export default function app() {
-  toolkit()
-  canvas()
-  utils()
-}
