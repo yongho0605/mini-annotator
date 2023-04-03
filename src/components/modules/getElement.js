23cf5f4e45da090ee5e1807e282f713b23f50faf
장용호 <yongho.jang@datamaker.io>
2023-04-03 11:27:24 +0900

    fix: Remove disabled El

--- src/components/modules/getElement.js
+++ src/components/modules/getElement.js
@@ -3,17 +3,3 @@ export const classSelectorEl = document.querySelector('#classSelector')
 export const annotatorEl = document.querySelector('.annotator')
 export const mainEl = document.querySelector('main')
 export const coordinateIndex = document.querySelector('.coordinateIndex')
-export const DOMEl = document.querySelector('.DOMCoordinate')
-export const canvasEl = document.querySelector('.DOMCoordinate')
-export const canvasOnImageEl = document.querySelector(
-  '.canvasOnImageCoordinate'
-)
-export const canvasTranslateEl = document.querySelector(
-  '.canvasTranslateCoordinate'
-)
-export const imageTranslateEl = document.querySelector(
-  '.imageTranslateCoordinate'
-)
-export const canvasScaleEl = document.querySelector('.canvasScaleCoordinate')
-export const canvasImageSize = document.querySelector('.canvasImageSize')
-export const naturalImageSize = document.querySelector('.naturalImageSize')
