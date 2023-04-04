c8803d6bf76fca05a0b0756b59991565649cb5de
장용호 <yongho.jang@datamaker.io>
2023-04-04 09:45:10 +0900

    feat: 이미지 비율 유지와 확대 축소 최대치 limit 제한 적용

--- src/components/Toolkit/activeZoom.js
+++ src/components/Toolkit/activeZoom.js
@@ -1,26 +1,44 @@
 import { getCanvasMousePosition } from '/src/components/canvas/canvasExport.js'
 import {
-  imageCanvas,
+  imgCanvas,
   guideLineCanvas,
-  imageCtx,
+  imgCtx,
 } from '/src/components/canvas/canvasExport.js'
+import CoordStore from '/src/store/coordStore.js'
 
-// FIXME: 확대 축소 먼저 구현
 export default function activeZoom(img) {
+  function roundFloatingPoint_2(num) {
+    return Number(num.toFixed(2))
+  }
   function wheel(e) {
     const { x, y } = getCanvasMousePosition(e, guideLineCanvas)
+    const minScale = 0.05
+    const maxScale = 30
+    let scale = e.deltaY > 0 ? 0.9 : 1.1
+    const imgTransform = imgCtx.getTransform()
+    const imgScaleX = roundFloatingPoint_2(imgTransform.a)
 
-    const scale = e.deltaY > 0 ? 0.9 : 1.1
-    console.log(e.deltaY)
+    imgCtx.clearRect(-10, -10, imgCanvas.width + 20, imgCanvas.height + 20)
 
-    imageCtx.clearRect(0, 0, imageCanvas.width, imageCanvas.height)
+    if (imgScaleX < minScale && scale === 0.9) {
+      scale = 1
+    } else if (imgScaleX > maxScale && scale === 1.1) {
+      scale = 1
+    }
 
-    imageCtx.translate(x, y)
-    imageCtx.scale(scale, scale)
-    imageCtx.translate(-x, -y)
+    imgCtx.translate(x, y)
+    console.log(imgTransform.e)
+    imgCtx.scale(scale, scale)
+    console.log(imgTransform.f)
+    imgCtx.translate(-x, -y)
 
-    imageCtx.drawImage(img, 0, 0)
+    imgCtx.drawImage(
+      img,
+      CoordStore.imgTranslate.x,
+      CoordStore.imgTranslate.y,
+      CoordStore.canvasImgSize.width,
+      CoordStore.canvasImgSize.height
+    )
   }
-
   guideLineCanvas.addEventListener('wheel', wheel)
 }
