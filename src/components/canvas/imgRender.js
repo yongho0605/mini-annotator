3205806d42f60b8b86a229e7d0d17bd6a6cde5e5
장용호 <yongho.jang@datamaker.io>
2023-04-04 09:45:29 +0900

    chore: Rename Func

--- src/components/canvas/imgRender.js
+++ src/components/canvas/imgRender.js
@@ -2,10 +2,9 @@ import ActiveGuideLine from '/src/components/toolkit/activeGuideLine.js'
 import ActiveZoom from '/src/components/toolkit/activeZoom.js'
 import ActivePan from '/src/components/toolkit/activePan.js'
 import ImgStore from '/src/store/imgStore.js'
-import InitializeCanvasImgSize from '/src/components/toolkit/resize/applyChangesOnResize.js'
+import InitCanvasImgSize from '/src/components/toolkit/resize/applyChangesOnResize.js'
 import { imgCanvas, imgCtx } from '/src/components/canvas/canvasExport.js'
 import { setCanvasSize } from '/src/components/canvas/canvasExport.js'
-import { annotatorEl } from '/src/components/modules/getElement.js'
 
 export default function imgCanvasRender() {
   setCanvasSize()
@@ -19,6 +18,7 @@ export default function imgCanvasRender() {
     ActiveGuideLine()
     ActiveZoom(img)
     ActivePan(img)
-    InitializeCanvasImgSize(img, imgCanvas, imgCtx, annotatorEl)
+
+    InitCanvasImgSize(img, imgCanvas, imgCtx)
   }
 }
