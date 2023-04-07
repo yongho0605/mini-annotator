3ee14f95cb787515ccd2b0920ebd92ec69f09058
장용호 <yongho.jang@datamaker.io>
2023-04-07 10:04:52 +0900

    feat: 여태껏 작업한것들, 아직은 미완성

--- src/components/Toolkit/activeZoom.js
+++ src/components/Toolkit/activeZoom.js
@@ -1,44 +1,68 @@
+import CoordStore from '/src/store/coordStore.js'
+import applyChangesOnResize from '/src/components/toolkit/resize/applyChangesOnResize.js'
 import { getCanvasMousePosition } from '/src/components/canvas/canvasExport.js'
 import {
   imgCanvas,
   guideLineCanvas,
   imgCtx,
 } from '/src/components/canvas/canvasExport.js'
-import CoordStore from '/src/store/coordStore.js'
 
 export default function activeZoom(img) {
-  function roundFloatingPoint_2(num) {
-    return Number(num.toFixed(2))
-  }
-  function wheel(e) {
-    const { x, y } = getCanvasMousePosition(e, guideLineCanvas)
-    const minScale = 0.05
-    const maxScale = 30
-    let scale = e.deltaY > 0 ? 0.9 : 1.1
-    const imgTransform = imgCtx.getTransform()
-    const imgScaleX = roundFloatingPoint_2(imgTransform.a)
+  const roundFloatingPoint_2 = (num) => Number(num.toFixed(2))
+  let currentScale = 1
+  const afterScale = null
+  const scaleArray = [currentScale, afterScale]
 
+  function onWheel(e) {
+    applyChangesOnResize(img, imgCanvas, imgCtx)
+    const { x, y } = getCanvasMousePosition(e, guideLineCanvas)
+    const { imgTranslate, imgSize } = CoordStore
+    const minScale = 0.1
+    const maxScale = 30
+    const scaleFactor = e.deltaY > 0 ? 0.9 : 1.1
     imgCtx.clearRect(-10, -10, imgCanvas.width + 20, imgCanvas.height + 20)
 
-    if (imgScaleX < minScale && scale === 0.9) {
-      scale = 1
-    } else if (imgScaleX > maxScale && scale === 1.1) {
-      scale = 1
+    applyTranslate()
+
+    function applyTranslate() {
+      if (scaleFactor === 0.9 && scaleArray[0] > minScale) {
+        compareScaledCondition()
+      } else if (scaleFactor === 1.1 && scaleArray[0] < maxScale) {
+        compareScaledCondition()
+      }
+      function compareScaledCondition() {
+        const reassignmentIndex = () =>
+          roundFloatingPoint_2(scaleArray[0] * scaleFactor)
+        scaleArray[0] = reassignmentIndex()
+        scaleArray[1] = reassignmentIndex()
+        if (e.deltaY === -0) {
+          scaleArray[0] = 1
+          scaleArray[1] = 1
+        }
+        const currentScale = scaleArray[0]
+        const getTranslateScaled = (size) => size - size * currentScale
+        const translateScaledWidth = getTranslateScaled(imgCanvas.width)
+        const translateScaledHeight = getTranslateScaled(imgCanvas.height)
+        const translateX = roundFloatingPoint_2(translateScaledWidth / 2)
+        const translateY = roundFloatingPoint_2(translateScaledHeight / 2)
+        imgCtx.setTransform(
+          currentScale,
+          0,
+          0,
+          currentScale,
+          translateX,
+          translateY
+        )
+        // imgCtx.translate(-translateX, -translateY)
+      }
     }
-
-    imgCtx.translate(x, y)
-    console.log(imgTransform.e)
-    imgCtx.scale(scale, scale)
-    console.log(imgTransform.f)
-    imgCtx.translate(-x, -y)
-
     imgCtx.drawImage(
       img,
-      CoordStore.imgTranslate.x,
-      CoordStore.imgTranslate.y,
-      CoordStore.canvasImgSize.width,
-      CoordStore.canvasImgSize.height
+      imgTranslate.x,
+      imgTranslate.y,
+      imgSize.width,
+      imgSize.height
     )
   }
-  guideLineCanvas.addEventListener('wheel', wheel)
+  guideLineCanvas.addEventListener('wheel', onWheel)
 }
