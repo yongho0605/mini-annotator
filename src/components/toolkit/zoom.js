261aa62c33da5dfa9f484d3232c91476a0794dd7
장용호 <yongho.jang@datamaker.io>
2023-04-12 19:57:00 +0900

    feat: Hold to zoom func

--- src/components/toolkit/zoom.js
+++ src/components/toolkit/zoom.js
@@ -1,5 +1,4 @@
 import CoordStore from '/src/store/coordStore.js'
-import { applyChangesOnResize } from '/src/components/toolkit/resize/resizeUtil.js'
 import { getCanvasMousePosition } from '/src/components/canvas/canvasExport.js'
 import {
   imgCanvas,
@@ -9,43 +8,48 @@ import {
 
 export default function applyZoom(img) {
   const scale = {
+    before: 1,
     current: 1,
-    after: null,
+    min: 0.1,
+    max: 100,
   }
+  const mouseCoordArr = []
 
   function onWheel(e) {
-    applyChangesOnResize(img, imgCanvas, imgCtx)
-    const { x, y } = getCanvasMousePosition(e, guideLineCanvas)
+    const { x, y } = getCanvasMousePosition(e, imgCanvas)
     const { imgTranslate, imgSize } = CoordStore
-    const minScale = 0.1
-    const maxScale = 100
     const scaleFactor = e.deltaY > 0 ? 0.9 : 1.1
     imgCtx.clearRect(-10, -10, imgCanvas.width + 20, imgCanvas.height + 20)
 
     applyTranslation()
 
     function applyTranslation() {
-      if (scaleFactor === 0.9 && scale.current > minScale) {
+      if (scaleFactor === 0.9 && scale.before > scale.min) {
         compareScaledCondition()
-      } else if (scaleFactor === 1.1 && scale.current < maxScale) {
+      } else if (scaleFactor === 1.1 && scale.before < scale.max) {
         compareScaledCondition()
       }
+
       function compareScaledCondition() {
-        scale.current = scale.current * scaleFactor
-        scale.after = scale.current * scaleFactor
+        scale.before = scale.current
+        scale.current = scale.before * scaleFactor
+
+        const currentMouseCoord = {
+          scale: scale.current,
+          x,
+          y,
+        }
         if (e.deltaY === 0) {
-          scale.current = 1
-          scale.after = 1
+          scale.current = scale.before
+          return
         }
-        const getComputedScaledSize = (size) => size - size * scale.current
-        const computedScaledSize = {
-          width: getComputedScaledSize(imgCanvas.width) / (imgCanvas.width / x),
-          height:
-            getComputedScaledSize(imgCanvas.height) / (imgCanvas.height / y),
-        }
+        mouseCoordArr.push(currentMouseCoord)
+        mouseCoordArr.length > 2 && mouseCoordArr.shift()
+
+        const getScaleComputedSize = (size) => size - size * scale.current
         const translate = {
-          x: computedScaledSize.width,
-          y: computedScaledSize.height,
+          x: getScaleComputedSize(imgCanvas.width) / 2,
+          y: getScaleComputedSize(imgCanvas.height) / 2,
         }
         imgCtx.setTransform(
           scale.current,
