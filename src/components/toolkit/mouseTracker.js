f731e8b83b633dba091dfe5866fb8c487f232ec4
장용호 <yongho.jang@datamaker.io>
2023-04-14 17:00:45 +0900

    feat: Add to new Coordinates and code Refactoring

--- src/components/toolkit/mouseTracker.js
+++ src/components/toolkit/mouseTracker.js
@@ -1,5 +1,9 @@
 import CoordStore from '/src/store/coordStore.js'
-import { imgCanvas, imgCtx } from '/src/components/canvas/canvasExport.js'
+import { getCanvasMousePosition } from '/src/components/canvas/canvasExport.js'
+import {
+  imgCanvas as canvas,
+  imgCtx as ctx,
+} from '/src/components/canvas/canvasExport.js'
 import {
   annotatorEl,
   coordinateIndex,
@@ -7,35 +11,44 @@ import {
 
 export default function chaseWholeCanvasCoordinate(img) {
   annotatorEl.addEventListener('mousemove', onMouseMove)
-  function onMouseMove() {
+  function onMouseMove(e) {
     const { width: imgWidth, height: imgHeight } = img
-    const transform = imgCtx.getTransform()
+    const transform = ctx.getTransform()
+    const { x, y } = getCanvasMousePosition(e, canvas)
+    const fixedSecondDecimalPoint = (computedValue) =>
+      Number(computedValue.toFixed(2))
 
-    const fixedSecondDecimalPoint = (num) => Number(num.toFixed(2))
-
-    CoordStore.canvasOnImg.x = CoordStore.canvas.x - CoordStore.imgTranslate.x
-    CoordStore.canvasOnImg.y = CoordStore.canvas.y - CoordStore.imgTranslate.y
+    CoordStore.canvas.x = x
+    CoordStore.canvas.y = y
+    CoordStore.imgOnCanvas.x = fixedSecondDecimalPoint(
+      CoordStore.canvas.x - CoordStore.imgTranslate.x
+    )
+    CoordStore.imgOnCanvas.y = fixedSecondDecimalPoint(
+      CoordStore.canvas.y - CoordStore.imgTranslate.y
+    )
     CoordStore.canvasTranslate.x = fixedSecondDecimalPoint(transform.e)
     CoordStore.canvasTranslate.y = fixedSecondDecimalPoint(transform.f)
     CoordStore.canvasScale.x = fixedSecondDecimalPoint(transform.a)
     CoordStore.canvasScale.y = fixedSecondDecimalPoint(transform.d)
     CoordStore.imgScale.x = fixedSecondDecimalPoint(
-      (imgCanvas.width * transform.a) / imgWidth
+      (canvas.width * transform.a) / imgWidth
     )
     CoordStore.imgScale.y = fixedSecondDecimalPoint(
-      (imgCanvas.height * transform.d) / imgHeight
+      (canvas.height * transform.d) / imgHeight
     )
     CoordStore.naturalImgSize.width = imgWidth
     CoordStore.naturalImgSize.height = imgHeight
 
     coordinateIndex.innerHTML = `
     <li>canvas:(${CoordStore.canvas.x}, ${CoordStore.canvas.y})</li>
-    <li>canvasOnImg:(${CoordStore.canvasOnImg.x}, ${CoordStore.canvasOnImg.y})</li>
+    <li>imgOnCanvas:(${CoordStore.imgOnCanvas.x}, ${CoordStore.imgOnCanvas.y})</li>
     <li>canvasTranslate:(${CoordStore.canvasTranslate.x}, ${CoordStore.canvasTranslate.y})</li>
     <li>imgTranslate:(${CoordStore.imgTranslate.x}, ${CoordStore.imgTranslate.y})</li>
+    <li>scaledImgTranslate: (${CoordStore.scaledImgTranslate.x}, ${CoordStore.scaledImgTranslate.y})</li>
     <li>canvasScale:(${CoordStore.canvasScale.x}, ${CoordStore.canvasScale.y})</li>
     <li>imgScale: (${CoordStore.imgScale.x}, ${CoordStore.imgScale.y})</li>
     <li>ImgSize width: ${CoordStore.imgSize.width} height: ${CoordStore.imgSize.height}</li>
+    <li>scaledImgSize width: ${CoordStore.scaledImgSize.width} height: ${CoordStore.scaledImgSize.height}</li>
     <li>naturalImgSize width: ${CoordStore.naturalImgSize.width} height: ${CoordStore.naturalImgSize.height}</li>
     `
   }
