587e2696fd8eea77dbd4d25f93fdd870aecf1611
장용호 <yongho.jang@datamaker.io>
2023-04-26 09:42:40 +0900

    feat: Add new image and apply annotator

--- src/components/canvas/imgRender.js
+++ src/components/canvas/imgRender.js
@@ -2,9 +2,7 @@ import applyAssembledCommonTools from '/src/assemble/toolkit/common.js'
 
 export default function imgCanvasRenderer() {
   const img = new Image()
-  // img.src = '/src/assets/images/mudeung.jpg'
-  // img.src = '/src/assets/images/musk.jpeg'
-  img.src = '/src/assets/images/tesla_model_S_Plaid.webp'
+  img.src = '/src/assets/images/elonMusk.jpeg'
   img.onload = () => {
     applyAssembledCommonTools(img)
   }
