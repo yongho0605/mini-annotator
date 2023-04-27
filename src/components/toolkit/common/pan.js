529c528273fb49c01df5af290debf024a3bfa119
장용호 <yongho.jang@datamaker.io>
2023-04-27 15:37:06 +0900

    feat: Fix a problem scale - pan - scale issue. but not fixed after panning and scaling

--- src/components/toolkit/common/pan.js
+++ src/components/toolkit/common/pan.js
@@ -4,11 +4,12 @@ import {
   imgCanvas,
   getCanvasMousePosition,
 } from '/src/components/canvas/canvasExport.js'
+import * as CanvasExport from '/src/components/canvas/canvasExport.js'
 import PanUtils from '/src/components/toolkit/utils/common/panUtil.js'
-import MouseButtons from '/src/modules/mouseButtons.js'
 import Store from '/src/store/store.js'
 import State from '/src/store/state/state.js'
 import Arithmetic from '/src/modules/Arithmetic.js'
+import MouseButtons from '/src/modules/mouseButtons.js'
 
 const { zoom } = Store
 const { pressed } = State
@@ -36,12 +37,17 @@ const pan = {
       }
     }
     function onMouseDown(evt) {
-      if (evt.button === MouseButtons.LEFT) {
-        State.pressed.mouse = true
+      // if (evt.button === MouseButtons.LEFT) {
+      //   State.pressed.mouse = true
+      //   Store.pan.initCoord = getCanvasMousePosition(evt, imgCanvas)
+      //   pressed.space && (Store.pan.initTransform = imgCtx.getTransform())
+      //   checkPressed()
+      // }
+      PanUtils.innerMouseEventHandler(evt, true, () => {
         Store.pan.initCoord = getCanvasMousePosition(evt, imgCanvas)
         pressed.space && (Store.pan.initTransform = imgCtx.getTransform())
         checkPressed()
-      }
+      })
     }
     function onMouseUp(evt) {
       if (evt.button === MouseButtons.LEFT) {
@@ -53,10 +59,32 @@ const pan = {
           conditionIdentifier('x') && (zoom.panedDistance.x = 0)
           conditionIdentifier('y') && (zoom.panedDistance.y = 0)
           Store.pan.movedArr.push({ ...zoom.panedDistance })
+          const panedTranslation = {
+            x: Arithmetic.sum2DCoord(Store.pan.movedArr, 'x'),
+            y: Arithmetic.sum2DCoord(Store.pan.movedArr, 'y'),
+          }
+          Store.zoom.panedTranslation = panedTranslation
         }
         State.pressed.mouse = false
         removeEvent()
       }
+      // PanUtils.innerMouseEventHandler(evt, false, () => {
+      //   if (pressed.space && (zoom.panedDistance.x || zoom.panedDistance.y)) {
+      //     const conditionIdentifier = (axis) =>
+      //       Store.pan.movedArr.length >= 1 &&
+      //       Store.pan.movedArr.at(-1)[axis] === zoom.panedDistance[axis]
+
+      //     conditionIdentifier('x') && (zoom.panedDistance.x = 0)
+      //     conditionIdentifier('y') && (zoom.panedDistance.y = 0)
+      //     Store.pan.movedArr.push({ ...zoom.panedDistance })
+      //     const panedTranslation = {
+      //       x: Arithmetic.sum2DCoord(Store.pan.movedArr, 'x'),
+      //       y: Arithmetic.sum2DCoord(Store.pan.movedArr, 'y'),
+      //     }
+      //     Store.zoom.panedTranslation = panedTranslation
+      //   }
+      //   removeEvent()
+      // })
     }
     function onKeyDown(evt) {
       PanUtils.innerKeyEventHandler(evt, true, checkPressed())
--- src/components/toolkit/utils/common/panUtil.js
+++ src/components/toolkit/utils/common/panUtil.js
@@ -1,13 +1,13 @@
 import Store from '/src/store/store.js'
 import State from '/src/store/state/state.js'
+import MouseButtons from '/src/modules/mouseButtons.js'
 import { guideLineCanvas as canvas } from '/src/components/canvas/canvasExport.js'
 
 const translate = { x: 0, y: 0 }
+const { pan, zoom } = Store
 
 const panUtils = {
-  applyChangesOnPan(originCurrentGLCoord, ctx) {
-    const { pan, zoom } = Store
-
+  applyChangesOnPan: (originCurrentGLCoord, ctx) => {
     const getComputedTranslation = (axis) =>
       originCurrentGLCoord[axis] -
       pan.initCoord[axis] +
@@ -17,15 +17,13 @@ const panUtils = {
     translate.y = Store.pan.translate.y = getComputedTranslation('y')
 
     if (zoom.scale.current !== 1 && (pan.translate.x || pan.translate.y)) {
-      Store.zoom.panedDistance.x = translate.x - pan.initTransform.e
-      Store.zoom.panedDistance.y = translate.y - pan.initTransform.f
-
-      Store.zoom.panedTranslation.x = zoom.panedDistance.x
-      Store.zoom.panedTranslation.y = zoom.panedDistance.y
+      Store.zoom.panedDistance.x =
+        (translate.x - pan.initTransform.e) / zoom.scale.current
+      Store.zoom.panedDistance.y =
+        (translate.y - pan.initTransform.f) / zoom.scale.current
+      Store.zoom.panedTranslation = zoom.panedDistance
     }
-
-    console.log(pan.translate, zoom.panedTranslation)
-
+    console.log(pan.translate)
     ctx.setTransform(
       zoom.scale.current,
       0,
@@ -36,10 +34,17 @@ const panUtils = {
     )
   },
 
-  innerKeyEventHandler(evt, boolean, callback) {
+  innerKeyEventHandler: (evt, boolean, callback) => {
     if (evt.code === 'Space') {
       State.pressed.space = boolean
       cursorStyleHandler()
+      callback
+    }
+  },
+
+  innerMouseEventHandler: (evt, boolean, callback) => {
+    if (evt.button === MouseButtons.LEFT) {
+      State.pressed.mouse = boolean
       callback
     }
   },
